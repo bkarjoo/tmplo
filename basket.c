9ce62446b770abcd864ccef9619246885bd4432a
Behrooz Karjoo <bkarjoo@gmail.com>
2019-10-29 10:11:40 -0400

    first commit

---
+++ basket.c
@@ -0,0 +1,23 @@
+#include "../etc/functions.h"
+basket_name = xyz
+basket_description = desc
+activate_dynamic_basket_rules = TRUE
+apply_dynamic_basket_rules_to_all_available_symbols = TRUE
+basket_rules =
+{
+
+}
+basket_symbols =
+{
+
+}
+
+basket_exclude =
+{
+
+}
+
+basket_htb =
+{
+
+}
